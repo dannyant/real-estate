eaaf7937973011608644f628bd4417d1aff78c84
dannyant <danny.antonetti@gmail.com>
2023-08-03 16:09:19 -0700

    send test to kafka

--- kafka/insert_kafka.py
+++ kafka/insert_kafka.py
@@ -1,10 +1,12 @@
 import json
 
+
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers="192.168.74.128:9092",
+    bootstrap_servers=["192.168.74.128:6667"],
     security_protocol="SSL",
+    api_version=(1, 0, 0),
     value_serializer=lambda v: json.dumps(v).encode('ascii'),
     key_serializer=lambda v: json.dumps(v).encode('ascii')
 )
