5348f4779b75efa9321f46f2e9432ca7c889e24b
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-11 10:31:20 -0700

    fixes issue

--- bin/apartments.sh
+++ bin/apartments.sh
@@ -1,1 +1,1 @@
-spark-submit --py-files /home/cloudera/.local/lib/python3.8/site-packages/xmltodict.py pullhttp/base_http_pull.py --master yarn   pullhttp/pull_apartments.py
+spark-submit --py-files pullhttp/base_http_pull.py --master yarn   pullhttp/pull_apartments.py
