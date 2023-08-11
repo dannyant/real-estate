867a0ed8e5b1f0e865c14b5d8bc27946cbd2bc23
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-11 10:36:03 -0700

    fixes spark submit

--- bin/apartments.sh
+++ bin/apartments.sh
@@ -1,1 +1,1 @@
-spark-submit --py-files pullhttp/base_http_pull.py --master yarn   pullhttp/pull_apartments.py
+spark-submit --deploy-mode cluster  --verbose --py-files pullhttp/base_http_pull.py --master yarn   pullhttp/pull_apartments.py
