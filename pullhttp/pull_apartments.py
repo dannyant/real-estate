02691d5121d2700d8b72783416dd12e616691100
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-12 11:17:54 -0700

    fixes spark submit

--- pullhttp/pull_apartments.py
+++ pullhttp/pull_apartments.py
@@ -8,19 +8,28 @@ from pyspark.shell import sc, spark
 from base_http_pull import pull_http
 
 
-def pull_sitemap_xml(sitemap):
+def pull_sitemap_xml(sitemap, url_list):
     print("Sitemap Processing " + str(sitemap))
     robots = pull_http(sitemap.strip(), as_text=False)
     robots_unzipped = gzip.decompress(robots).decode('utf-8')
     raw_robots = xmltodict.parse(robots_unzipped)
-    properties_zip_url = raw_robots["sitemapindex"]["sitemap"]["loc"]
-    properties_zipped = pull_http(properties_zip_url, as_text=False)
-    properties_unzipped = gzip.decompress(properties_zipped).decode('utf-8')
-    properties_xml = xmltodict.parse(properties_unzipped)
-    properties_data = properties_xml["urlset"]["url"]
-    print(properties_data)
-    print("Sitemap Done Processing " + str(properties_data))
-    return properties_data
+    properties_zip_url = raw_robots["sitemapindex"]["sitemap"]
+    if type(properties_zip_url) is dict:
+        properties_zip_url = [properties_zip_url]
+
+    for prop_url_dict in properties_zip_url:
+        loc_url = prop_url_dict["loc"]
+        properties_zipped = pull_http(loc_url, as_text=False)
+        properties_unzipped = gzip.decompress(properties_zipped).decode('utf-8')
+        properties_xml = xmltodict.parse(properties_unzipped)
+        for urlset in properties_xml:
+            url_val = properties_xml[urlset]
+            urls = url_val["url"]
+            for url_dict in urls:
+                new_dict = {}
+                new_dict["url"] = url_dict["loc"]
+                new_dict["last_mod"] = url_dict["lastmod"]
+                url_list.append(new_dict)
 
 def main():
     print("Sitemap Startup")
@@ -30,7 +39,9 @@ def main():
     robots_url = p.findall(robots)
     urls = []
     for robot in robots_url:
-        urls += pull_sitemap_xml(robot)
+        if ".gz" not in robots:
+            continue
+        pull_sitemap_xml(robot, urls)
 
     myrdd = sc.parallelize([urls])
     df = spark.createDataFrame(data=myrdd, schema = schema)
---
+++ schema/apartments_com.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS apartments_property (
+      url VARCHAR NOT NULL,
+      site_last_mod VARCHAR NOT NULL,
+      hbase_url VARCHAR,
+      last_downloaded VARCHAR,
+      population BIGINT
+      CONSTRAINT url_pk PRIMARY KEY (url));
