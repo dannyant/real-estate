7022633a1642c4df2681379e354be06683877c87
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-12 20:39:40 -0700

    fixes spark submit

--- pullhttp/pull_apartments.py
+++ pullhttp/pull_apartments.py
@@ -9,22 +9,14 @@ from base_http_pull import pull_http
 
 
 def pull_sitemap_xml(sitemap, url_list):
-    print("Sitemap Processing " + str(sitemap))
     robots = pull_http(sitemap.strip(), as_text=False)
     robots_unzipped = gzip.decompress(robots).decode('utf-8')
-    print("RAW RETURN " + str(robots_unzipped))
     raw_robots = xmltodict.parse(robots_unzipped)
     properties_zip_url = raw_robots["sitemapindex"]["sitemap"]
-    print("DIcT TYPE = " + str(dict))
     if isinstance(properties_zip_url, dict):
-        print("DICT = " + str(properties_zip_url))
         properties_zip_url = [properties_zip_url]
-    else:
-        print(isinstance(properties_zip_url, dict))
-        print("NOT DICT = " + str(type(properties_zip_url)))
 
     for prop_url_dict in properties_zip_url:
-        print("ZIP URL = " + str(prop_url_dict))
         loc_url = prop_url_dict["loc"]
         properties_zipped = pull_http(loc_url, as_text=False)
         properties_unzipped = gzip.decompress(properties_zipped).decode('utf-8')
@@ -39,7 +31,6 @@ def pull_sitemap_xml(sitemap, url_list):
                 url_list.append(new_dict)
 
 def main():
-    print("Sitemap Startup")
     url = "https://www.apartments.com/robots.txt"
     robots = pull_http(url)
     p = re.compile('Sitemap: (.*)')
