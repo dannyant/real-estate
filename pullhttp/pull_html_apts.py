a24dd95c3ce2a9e5a6c5ac7acbc260dbc7134fae
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-26 11:52:39 -0700

    fixes spark submit

--- pullhttp/pull_html_apts.py
+++ pullhttp/pull_html_apts.py
@@ -2,13 +2,14 @@ import sys
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType, StructType, StructField
+from pyspark.sql.types import StringType
 
-import urllib.request
+from base_http_pull import pull_http
+
 
 def download_url_content(url):
     try:
-        response = urllib.request.urlopen(url)
+        response = pull_http(url)
         print("response = " + str(len(response)))
         return response.read().decode('utf-8')  # Assuming UTF-8 encoding
     except Exception as e:
@@ -21,19 +22,25 @@ def main():
     df = spark.read.format("org.apache.phoenix.spark").option("table", "apartments_property")\
         .option("zkUrl", "namenode:2181").load()
 
+    print("got data")
     # Register UDF to download content
     download_udf = udf(download_url_content, StringType())
     # Add a new column with downloaded content
+    print("setup udf")
+    df = df.limit(10)
+
+    print("limit df")
     df_with_content = df.withColumn("html_content", download_udf(df["url"]))
-    df_with_content = df_with_content.limit(10)
     # Show the DataFrame with downloaded content
     df_with_content.show(truncate=False)
+    print("shown")
 
     df_with_content.write.format("org.apache.phoenix.spark") \
         .mode("overwrite") \
         .option("table", "apartments_property") \
         .option("zkUrl", "192.168.1.162:2181") \
         .save()
+    print("saved")
 
 
 if __name__ == "__main__":
