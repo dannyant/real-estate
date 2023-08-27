176455ecc28fcf7e868d590f40fb7fb43a7f0d71
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-27 09:36:33 -0700

    fixes spark submit

--- pullhttp/pull_http_by_sql.py
+++ pullhttp/pull_http_by_sql.py
@@ -1,19 +1,20 @@
-from datetime import date
+import time
+from datetime import date, datetime
 
 import phoenixdb.cursor
 from base_http_pull import pull_http
 
-database_url = 'http://192.168.1.162:8765/'
+database_url = 'http://namenode:8765/'
 conn = phoenixdb.connect(database_url, autocommit=True)
 
-cursor = conn.cursor(cursor_factory=phoenixdb.cursor.DictCursor)
-#cursor.execute("SELECT * FROM apartments_property WHERE last_downloaded is null")
-#cursor.execute("SELECT * FROM PARCEL_INFO")
-cursor.execute("SELECT * FROM tax_info")
 
+cursor = conn.cursor(cursor_factory=phoenixdb.cursor.DictCursor)
+cursor.execute("SELECT * FROM apartments_property WHERE last_downloaded is null limit 100")
 all_url_dicts = cursor.fetchall()
 print("count = " + str(len(all_url_dicts)))
 for url_dict in all_url_dicts:
     url = url_dict["URL"]
     content = pull_http(url)
-    cursor.execut
+    cursor.execute("UPSERT INTO apartments_property (URL, HTML_CONTENTS, LAST_DOWNLOADED) VALUES (?, ?, ?)", (url, content, str(datetime.now())))
+    time.sleep(10)
+
