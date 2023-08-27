93241b52a684b5ebbf558e7a551d7007ef339858
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-08-27 09:37:50 -0700

    fixes spark submit

--- pullhttp/pull_http_by_sql.py
+++ pullhttp/pull_http_by_sql.py
@@ -7,14 +7,16 @@ from base_http_pull import pull_http
 database_url = 'http://namenode:8765/'
 conn = phoenixdb.connect(database_url, autocommit=True)
 
-
-cursor = conn.cursor(cursor_factory=phoenixdb.cursor.DictCursor)
-cursor.execute("SELECT * FROM apartments_property WHERE last_downloaded is null limit 100")
-all_url_dicts = cursor.fetchall()
-print("count = " + str(len(all_url_dicts)))
-for url_dict in all_url_dicts:
-    url = url_dict["URL"]
-    content = pull_http(url)
-    cursor.execute("UPSERT INTO apartments_property (URL, HTML_CONTENTS, LAST_DOWNLOADED) VALUES (?, ?, ?)", (url, content, str(datetime.now())))
-    time.sleep(10)
+isempty = False
+while not isempty:
+    cursor = conn.cursor(cursor_factory=phoenixdb.cursor.DictCursor)
+    cursor.execute("SELECT * FROM apartments_property WHERE last_downloaded is null limit 100")
+    all_url_dicts = cursor.fetchall()
+    isempty = len(all_url_dicts) == 0
+    print("count = " + str(len(all_url_dicts)))
+    for url_dict in all_url_dicts:
+        url = url_dict["URL"]
+        content = pull_http(url)
+        cursor.execute("UPSERT INTO apartments_property (URL, HTML_CONTENTS, LAST_DOWNLOADED) VALUES (?, ?, ?)", (url, content, str(datetime.now())))
+        time.sleep(10)
 
