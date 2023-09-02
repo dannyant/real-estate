6a7f91d1622e96f10c4d206b9564cc15bca6db49
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-02 16:06:18 -0700

    fixes spark submit

--- rolls/calc_agg.py
+++ rolls/calc_agg.py
@@ -7,13 +7,20 @@ def main():
     df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") \
         .option("zkUrl", "namenode:2181").load()
 
-    df = df.groupby("COUNTY", "PARCEL_ID", "USE_TYPE")
-    df = df.agg(countDistinct("OWNER_NAME").alias("DISTINCT_OWNERS"),
+    df_groupby_use_type = df.groupby("COUNTY", "PARCEL_ID", "USE_TYPE")
+    df_groupby_use_type = df_groupby_use_type.agg(countDistinct("OWNER_NAME").alias("DISTINCT_OWNERS"),
                 countDistinct("MA_STREET_ADDRESS").alias("DISTINCT_OWNER_ADDRESS"))
-    df.show(4)
-    df.write.format("org.apache.phoenix.spark") \
+    df_groupby_use_type.write.format("org.apache.phoenix.spark") \
         .mode("overwrite") \
         .option("table", "ROLL_AGG_INFO") \
+        .option("zkUrl", "namenode:2181") \
+        .save()
+
+    df_groupby_owner = df.groupby("OWNER_NAME", "MA_STREET_ADDRESS", "MA_CITY_STATE")
+    df_groupby_owner = df_groupby_owner.agg(countDistinct("PARCEL_ID").alias("PARCEL_COUNT"))
+    df_groupby_owner.write.format("org.apache.phoenix.spark") \
+        .mode("overwrite") \
+        .option("table", "OWNER_NAME_PROPERTY_COUNT") \
         .option("zkUrl", "namenode:2181") \
         .save()
 
