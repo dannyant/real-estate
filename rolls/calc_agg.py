54149d90a61899e80cffda14e4c2e421c34500c2
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-02 12:20:37 -0700

    fixes spark submit

--- rolls/calc_agg.py
+++ rolls/calc_agg.py
@@ -8,9 +8,14 @@ def main():
         .option("zkUrl", "namenode:2181").load()
 
     df = df.groupby("COUNTY", "PARCEL_ID")
-    df = df.agg(countDistinct('OWNER_NAME'))
     df = df.agg(countDistinct("OWNER_NAME").alias("DISTINCT_OWNERS"),
                 countDistinct("MA_STREET_ADDRESS").alias("DISTINCT_OWNER_ADDRESS"))
     df.show(4)
+    df.write.format("org.apache.phoenix.spark") \
+        .mode("overwrite") \
+        .option("table", "ROLL_AGG_INFO") \
+        .option("zkUrl", "namenode:2181") \
+        .save()
+
 
 main()
