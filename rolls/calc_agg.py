4963dfd0b7ee04c073859284e0804cf02c5c82ae
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-02 12:34:51 -0700

    fixes spark submit

--- rolls/calc_agg.py
+++ rolls/calc_agg.py
@@ -7,7 +7,7 @@ def main():
     df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") \
         .option("zkUrl", "namenode:2181").load()
 
-    df = df.groupby("COUNTY", "PARCEL_ID")
+    df = df.groupby("COUNTY", "PARCEL_ID", "USE_TYPE")
     df = df.agg(countDistinct("OWNER_NAME").alias("DISTINCT_OWNERS"),
                 countDistinct("MA_STREET_ADDRESS").alias("DISTINCT_OWNER_ADDRESS"))
     df.show(4)
