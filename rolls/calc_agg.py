a07752a93e96673ea9494ad49f60deb3f43ca551
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-02 16:14:04 -0700

    fixes spark submit

--- rolls/calc_agg.py
+++ rolls/calc_agg.py
@@ -1,11 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import countDistinct
-
+from assessment_rolls import use_code_type
 
 def main():
     spark = SparkSession.builder.appName("CalcAgg").getOrCreate()
     df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") \
         .option("zkUrl", "namenode:2181").load()
+    df = df.withColumn("USE_TYPE", use_code_type(df["USE_CODE"])) \
+
 
     df_groupby_use_type = df.groupby("COUNTY", "PARCEL_ID", "USE_TYPE")
     df_groupby_use_type = df_groupby_use_type.agg(countDistinct("OWNER_NAME").alias("DISTINCT_OWNERS"),
@@ -16,7 +18,7 @@ def main():
         .option("zkUrl", "namenode:2181") \
         .save()
 
-    df_groupby_owner = df.groupby("OWNER_NAME", "MA_STREET_ADDRESS", "MA_CITY_STATE")
+    df_groupby_owner = df.groupby("OWNER_NAME", "MA_STREET_ADDRESS", "MA_CITY_STATE", "USE_TYPE")
     df_groupby_owner = df_groupby_owner.agg(countDistinct("PARCEL_ID").alias("PARCEL_COUNT"))
     df_groupby_owner.write.format("org.apache.phoenix.spark") \
         .mode("overwrite") \
