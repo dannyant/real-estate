0fee1d5d342f920f7b1f596aa854f9f5ddfb8f58
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-02 23:49:20 -0700

    fixes spark submit

--- rolls/assessment_rolls.py
+++ rolls/assessment_rolls.py
@@ -327,7 +327,11 @@ def main():
                     "TAXES_IMPROVEMENT_VALUE_LIST", "TAXES_IMPROVEMENT_VALUE_LIST", "CLCA_LAND_VALUE_LIST",
                     "CLCA_IMPROVEMENT_VALUE_LIST", "FIXTURES_VALUE_LIST", "ADDRESS_STREET_NAME_LIST")
 
-        df_groupby_parcel.show(10)
+        df.write.format("org.apache.phoenix.spark") \
+                .mode("overwrite") \
+                .option("table", "ROLL_INFO_AGG") \
+                .option("zkUrl", "namenode:2181") \
+                .save()
 
 
 
