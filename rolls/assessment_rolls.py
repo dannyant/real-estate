befcf02799d0c16d1ecadc141e66e3ec321ddafc
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-03 19:46:06 -0700

    fixes spark submit

--- rolls/assessment_rolls.py
+++ rolls/assessment_rolls.py
@@ -165,6 +165,26 @@ def is_state_change(state_list):
         return last_city != prev_city
     return False
 
+def newly_different_city(city_lst, ma_city_lst):
+    if len(city_lst) >= 1 and len(ma_city_lst) > 1:
+        last_city = city_lst[-1]
+        last_ma_city = ma_city_lst[-1]
+        prev_ma_city = ma_city_lst[-2]
+
+        return last_city != last_ma_city and last_city == prev_ma_city
+    return False
+
+def newly_different_address(address_num_lst, address_street_lst, ma_address_lst):
+    if len(address_num_lst) >= 1 and len(address_street_lst) >= 1 and len(ma_address_lst) > 1:
+        last_address_num = address_num_lst[-1]
+        last_address_street = address_street_lst[-1]
+        last_ma_address = ma_address_lst[-1]
+        prev_ma_address = ma_address_lst[-2]
+
+        return last_address_num in prev_ma_address and last_address_street in prev_ma_address and \
+            (last_address_num not in last_ma_address or last_address_street not in last_ma_address)
+    return False
+
 def split_address_get_state(val):
     val = val.strip()
     split = val.split(" ")
@@ -204,93 +224,8 @@ is_owner_change_udf = udf(is_owner_change, BooleanType())
 is_street_change_udf = udf(is_street_change, BooleanType())
 is_city_change_udf = udf(is_city_change, BooleanType())
 is_state_change_udf = udf(is_state_change, BooleanType())
-
-def main_old():
-    spark = SparkSession.builder.appName("ProcessRolls").getOrCreate()
-    df = spark.read.csv("hdfs://namenode:8020/user/spark/apartments/rolls", sep="\t", schema=schema)
-    df = df.withColumn("COUNTY", alameda_udf())\
-        .withColumn("PARCEL_ID", trimstr(df["APN_SHORT"])) \
-        .withColumn("USE_TYPE", use_code_type(df["USE_CODE"])) \
-        .withColumn("STREET_NUM", upperstr(df["ADDRESS_STREET_NUM"])) \
-        .withColumn("UNIT_NUM", upperstr(df["ADDRESS_UNIT_NUM"])) \
-        .withColumn("STREET_NAME", upperstr(df["ADDRESS_STREET_NAME"])) \
-        .withColumn("CITY", upperstr(df["ADDRESS_CITY"])) \
-        .withColumn("ZIP", upperstr(df["ADDRESS_ZIP"])) \
-        .withColumn("ZIP_EXTENSION", upperstr(df["ADDRESS_ZIP_EXTENSION"])) \
-        .withColumn("USE_TYPE", use_code_type(df["USE_CODE"]))
-
-
-    apn_df = df.select("PARCEL_ID", "COUNTY")
-    apn_df.write.format("org.apache.phoenix.spark") \
-        .mode("overwrite") \
-        .option("table", "PARCEL_INFO") \
-        .option("zkUrl", "namenode:2181") \
-        .save()
-
-    tax_df = df.select("PARCEL_ID", "COUNTY", "USE_TYPE")
-    tax_df = tax_df.filter("USE_TYPE = 'COMMERCIAL_RESIDENTIAL'")
-    tax_df.write.format("org.apache.phoenix.spark") \
-        .mode("overwrite") \
-        .option("table", "tax_info") \
-        .option("zkUrl", "namenode:2181") \
-        .save()
-
-    address_df = df.withColumn("STREET_NUMBER", upperstr(df["ADDRESS_STREET_NUM"])) \
-        .withColumn("UNIT_NUMBER", upperstr(df["ADDRESS_UNIT_NUM"])) \
-        .withColumn("STREET_NAME", upperstr(df["ADDRESS_STREET_NAME"])) \
-        .withColumn("CITY", upperstr(df["ADDRESS_CITY"])) \
-        .withColumn("ZIPCODE", upperstr(df["ADDRESS_ZIP"])) \
-        .withColumn("ZIPCODE_EXTENSION", upperstr(df["ADDRESS_ZIP_EXTENSION"])) \
-        .withColumn("USE_TYPE", use_code_type(df["USE_CODE"]))
-
-    address_df = address_df.select("PARCEL_ID", "COUNTY", "STREET_NUMBER", "UNIT_NUMBER", "STREET_NAME",
-                           "CITY", "ZIPCODE", "ZIPCODE_EXTENSION", "USE_TYPE")
-    address_df.write.format("org.apache.phoenix.spark") \
-        .mode("overwrite") \
-        .option("table", "ADDRESS_INFO") \
-        .option("zkUrl", "namenode:2181") \
-        .save()
-
-    owner_df = df.withColumn("OWNER_NAME", upperstr(df["OWNER_NAME"])) \
-        .withColumn("STREET_ADDRESS", upperstr(df["MA_STREET_ADDRESS"])) \
-        .withColumn("UNIT_NUMBER", upperstr(df["MA_UNIT_NUMBER"])) \
-        .withColumn("ZIPCODE", upperstr(df["MA_ZIP_CODE"])) \
-        .withColumn("ZIPCODE_EXTENSION", upperstr(df["MA_ZIP_CODE_EXTENSION"])) \
-        .withColumn("CARE_OF", upperstr(df["MA_CARE_OF"])) \
-        .withColumn("CITY", split_city(df["MA_CITY_STATE"])) \
-        .withColumn("STATE", split_state(df["MA_CITY_STATE"])) \
-        .withColumn("ATTN_NAME", upperstr(df["MA_ATTN_NAME"]))
-
-    owner_df = owner_df.select("OWNER_NAME", "STREET_ADDRESS", "CITY", "STATE", "UNIT_NUMBER",
-                         "ZIPCODE", "ZIPCODE_EXTENSION", "CARE_OF", "ATTN_NAME")
-    owner_df.write.format("org.apache.phoenix.spark") \
-        .mode("overwrite") \
-        .option("table", "OWNER_INFO") \
-        .option("zkUrl", "namenode:2181") \
-        .save()
-
-    value_df = df.select("COUNTY", "PARCEL_ID", "TAXES_LAND_VALUE", "TAXES_IMPROVEMENT_VALUE",
-                         "CLCA_LAND_VALUE", "CLCA_IMPROVEMENT_VALUE", "FIXTURES_VALUE", "PERSONAL_PROPERTY_VALUE",
-                         "HPP_VALUE", "HOMEOWNERS_EXEMPTION_VALUE", "OTHER_EXEMPTION_VALUE", "NET_TOTAL_VALUE",
-                         "LAST_DOC_PREFIX", "LAST_DOC_SERIES", "LAST_DOC_DATE", "LAST_DOC_INPUT_DATE")
-
-    value_df = value_df.withColumn("TAXES_LAND_VALUE", to_int_conv(df["TAXES_LAND_VALUE"])) \
-        .withColumn("TAXES_IMPROVEMENT_VALUE", to_int_conv(df["TAXES_IMPROVEMENT_VALUE"])) \
-        .withColumn("CLCA_LAND_VALUE", to_int_conv(df["CLCA_LAND_VALUE"])) \
-        .withColumn("CLCA_IMPROVEMENT_VALUE", to_int_conv(df["CLCA_IMPROVEMENT_VALUE"])) \
-        .withColumn("FIXTURES_VALUE", to_int_conv(df["FIXTURES_VALUE"])) \
-        .withColumn("PERSONAL_PROPERTY_VALUE", to_int_conv(df["PERSONAL_PROPERTY_VALUE"])) \
-        .withColumn("HPP_VALUE", to_int_conv(df["HPP_VALUE"])) \
-        .withColumn("HOMEOWNERS_EXEMPTION_VALUE", to_int_conv(df["HOMEOWNERS_EXEMPTION_VALUE"])) \
-        .withColumn("OTHER_EXEMPTION_VALUE", to_int_conv(df["OTHER_EXEMPTION_VALUE"])) \
-        .withColumn("NET_TOTAL_VALUE", to_int_conv(df["NET_TOTAL_VALUE"]))
-
-
-    value_df.write.format("org.apache.phoenix.spark") \
-        .mode("overwrite") \
-        .option("table", "VALUE_INFO") \
-        .option("zkUrl", "namenode:2181") \
-        .save()
+newly_different_city_udf = udf(newly_different_city, BooleanType())
+newly_different_address_udf = udf(newly_different_address, BooleanType())
 
 def main():
     spark = SparkSession.builder.appName("ProcessRolls").getOrCreate()
@@ -423,6 +358,11 @@ def main():
             .withColumn("MA_STREET_ADDRESS_CHANGE", is_street_change_udf(df_groupby_parcel["MA_STREET_ADDRESS_LIST"])) \
             .withColumn("MA_CITY_CHANGE", is_city_change_udf(df_groupby_parcel["MA_CITY_LIST"])) \
             .withColumn("MA_STATE_CHANGE", is_state_change_udf(df_groupby_parcel["MA_STATE_LIST"])) \
+            .withColumn("MA_DIFFERENT_CITY", newly_different_city_udf(df_groupby_parcel["ADDRESS_CITY_LIST"],
+                                                                      df_groupby_parcel["MA_CITY_LIST"])) \
+            .withColumn("MA_DIFFERENT_ADDR", newly_different_address_udf(df_groupby_parcel["ADDRESS_STREET_NUM_LIST"],
+                                                                         df_groupby_parcel["ADDRESS_STREET_NAME_LIST"],
+                                                                         df_groupby_parcel["MA_STREET_ADDRESS_LIST"])) \
             .withColumn("SOURCE_INFO_DATE", file_map[file]())
 
         df_groupby_parcel.write.format("org.apache.phoenix.spark") \
