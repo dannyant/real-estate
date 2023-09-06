0d59fb4b23ce5bf2c889bbf38dade75794341767
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-06 01:26:09 -0700

    fixes spark submit

--- rolls/owner_agg.py
+++ rolls/owner_agg.py
@@ -11,10 +11,11 @@ owner_df = df.select("COUNTY", "SOURCE_INFO_DATE", "OWNER_NAME", "STREET_ADDRESS
 owner_df = owner_df.groupBy("COUNTY", "SOURCE_INFO_DATE", "OWNER_NAME", "STREET_ADDRESS", "CITY", "STATE", "UNIT_NUMBER",
                            "ZIPCODE", "ZIPCODE_EXTENSION", "CARE_OF", "ATTN_NAME")
 
-owner_df = owner_df.agg(collect_list("PARCEL_ID").alias("OWNED_PROPERTIES"),
+owner_df = owner_df.agg(collect_list("PARCEL_ID").alias("OWNED_PROPERTIES"))
 
 owner_df.write.format("org.apache.phoenix.spark") \
     .mode("overwrite") \
     .option("table", "OWNER_INFO") \
     .option("zkUrl", "namenode:2181") \
     .save()
+
