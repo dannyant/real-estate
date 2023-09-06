3606477563dd9768b4a460d7f10e3a5c931670e5
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-06 01:35:20 -0700

    fixes spark submit

--- rolls/owner_agg.py
+++ rolls/owner_agg.py
@@ -6,13 +6,15 @@ df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") 
     .option("zkUrl", "namenode:2181").load()
 
 owner_df = df.select("COUNTY", "SOURCE_INFO_DATE", "OWNER_NAME", "MA_STREET_ADDRESS", "MA_CITY", "MA_STATE", "MA_UNIT_NUMBER",
-                           "MA_ZIP_CODE", "MA_ZIP_CODE_EXTENSION", "CARE_OF", "ATTN_NAME", "PARCEL_ID")
+                           "MA_ZIP_CODE", "MA_ZIP_CODE_EXTENSION", "MA_CARE_OF", "MA_ATTN_NAME", "PARCEL_ID")
 
 owner_df = owner_df.withColumnRenamed("MA_STREET_ADDRESS", "STREET_ADDRESS") \
                    .withColumnRenamed("MA_CITY", "CITY") \
                    .withColumnRenamed("MA_UNIT_NUMBER", "UNIT_NUMBER") \
-                   .withColumnRenamed("MA_ZIPCODE", "ZIPCODE") \
-                   .withColumnRenamed("MA_ZIPCODE_EXTENSION", "ZIPCODE_EXTENSION") \
+                   .withColumnRenamed("MA_ZIP_CODE", "ZIPCODE") \
+                   .withColumnRenamed("MA_ZIP_CODE_EXTENSION", "ZIPCODE_EXTENSION") \
+                   .withColumnRenamed("MA_CARE_OF", "CARE_OF") \
+                   .withColumnRenamed("MA_ATTN_NAME", "ATTN_NAME") \
                    .withColumnRenamed("MA_STATE", "STATE") \
 
 owner_df = owner_df.groupBy("COUNTY", "SOURCE_INFO_DATE", "OWNER_NAME", "STREET_ADDRESS", "CITY", "STATE", "UNIT_NUMBER",
