1aaee2484d71b6e72222386fdeaafacc47707486
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-08 01:15:10 -0700

    fixes spark submit

--- rolls/assessment_rolls_sacramento.py
+++ rolls/assessment_rolls_sacramento.py
@@ -49,23 +49,29 @@ def sacramento():
 def create_parcel_id(mapb, pg, pcl, psub):
     return mapb + pg + pcl + psub
 
+def source_info_2023():
+    return "2023"
+
 trimstr = udf(trim, StringType())
 upperstr = udf(upper, StringType())
 sacramento_udf = udf(sacramento, StringType())
 create_parcel_id_udf = udf(create_parcel_id, StringType())
+source_info_2023_udf = udf(source_info_2023, StringType())
 
 
 def main():
     spark = SparkSession.builder.appName("ProcessRolls").getOrCreate()
     separators = {"2018_secured_roll.txt" : ';', "2019_secured_roll.txt" : ';', "2020_secured_roll.txt" : ';',
                   "2021_secured_roll.txt" : ';', "2022_secured_roll.txt" : ';', "2023_secured_roll.txt" : ';'}
+    udfs = {"2023_secured_roll.txt" : source_info_2023_udf}
     #for file in ["2018_secured_roll.txt", "2019_secured_roll.txt", "2020_secured_roll.txt", "2021_secured_roll.txt", "2022_secured_roll.txt", "2023_secured_roll.txt"]:
     for file in ["2023_secured_roll.txt"]:
         loc = "hdfs://namenode:8020/user/spark/apartments/rolls/sacramento/" + file
         df = spark.read.csv(loc, sep=separators[file], schema=schema)
         df = df.filter("MAPB != 'MAPB'")
         df = df.withColumn("COUNTY", sacramento_udf()) \
-               .withColumn("PARCEL_ID", create_parcel_id_udf(df["MAPB"], df["PG"], df["PCL"], df["PSUB"]))
+               .withColumn("PARCEL_ID", create_parcel_id_udf(df["MAPB"], df["PG"], df["PCL"], df["PSUB"])) \
+               .withColumn("SOURCE_INFO_DATE", udfs[file]())
         df.show(5)
 
 
