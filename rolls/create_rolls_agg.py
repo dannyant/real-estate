cbd2010b5ba9503f9997d67f56f1cd7229c787dc
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-09 20:37:21 -0700

    fixes spark submit

--- rolls/create_rolls_agg.py
+++ rolls/create_rolls_agg.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
 
 spark = SparkSession.builder.appName("ProcessRolls").getOrCreate()
 df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") \
     .option("zkUrl", "namenode:2181").load()
 
 df = df.groupby("COUNTY", "SOURCE_INFO_DATE")
-df = df.agg()
-list = df.collect.toList
+df = df.agg(count("*"))
+df_list = df.collect.toList
 
-print(list)
+print(df_list)
