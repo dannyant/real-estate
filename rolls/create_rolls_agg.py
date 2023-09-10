ea84f785a1096c1639ae68ece86d5a00186e2a98
Daniel Antonetti <dantonetti@marinsoftware.com>
2023-09-09 20:35:04 -0700

    fixes spark submit

--- rolls/create_rolls_agg.py
+++ rolls/create_rolls_agg.py
@@ -5,7 +5,7 @@ df = spark.read.format("org.apache.phoenix.spark").option("table", "ROLL_INFO") 
     .option("zkUrl", "namenode:2181").load()
 
 df = df.groupby("COUNTY", "SOURCE_INFO_DATE")
-df = df.select("COUNTY", "SOURCE_INFO_DATE")
+df = df.agg()
 list = df.collect.toList
 
 print(list)
